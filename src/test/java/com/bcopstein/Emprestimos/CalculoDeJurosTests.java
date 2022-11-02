9635af996d229373a044551baed0d79fcfe67ef4
William Schneider <william.lima.schneider@gmail.com>
2022-11-02 19:02:28 -0300

    CalculoDeJurosTest

--- src/test/java/com/bcopstein/Emprestimos/CalculoDeJurosTests.java
+++ src/test/java/com/bcopstein/Emprestimos/CalculoDeJurosTests.java
@@ -1,6 +1,5 @@
 package com.bcopstein.Emprestimos;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.api.BeforeEach;
